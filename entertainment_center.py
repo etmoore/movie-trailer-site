96e0b5fb646fa48b947764ebc381d0031f980b0f
etmoore <etmoore@gmail.com>
2016-09-08 23:46:24 -0600

    create instance of Avatar movie

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,3 +4,9 @@ toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                    "A marine on an alien planet",
+                    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
