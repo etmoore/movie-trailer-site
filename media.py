8f00a73e27e0acadd9b326fe311cddfd06a8ddd5
etmoore <etmoore@gmail.com>
2016-09-08 23:38:49 -0600

    create Movie class and instantiate Toy Story object

---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie():
+
+    def __init__(self, title, storyline, poster_image_url, youtube_trailer_url):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.youtube_trailer_url = youtube_trailer_url
