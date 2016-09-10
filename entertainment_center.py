920b3230733dd3b452f9184312eabfef50cd23a6
etmoore <etmoore@gmail.com>
2016-09-10 17:50:29 -0600

    add comments and docstrings, fix pep8 warnings

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,25 +1,26 @@
 import media
 import fresh_tomatoes
 
+# Create instances of Movie
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
-                    "A marine on an alien planet",
-                    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 eternal_sunshine = media.Movie("Eternal Sunshine of the Spotless Mind",
-                                "Lovers try to forget their past and start over",
-                                "https://upload.wikimedia.org/wikipedia/en/6/62/Eternal_sunshine_of_the_spotless_mind_ver3.jpg",
-                                "https://www.youtube.com/watch?v=rb9a00bXf-U")
+                               "Lovers try to forget their past and start over",
+                               "https://upload.wikimedia.org/wikipedia/en/6/62/Eternal_sunshine_of_the_spotless_mind_ver3.jpg",
+                               "https://www.youtube.com/watch?v=rb9a00bXf-U")
 
 garden_state = media.Movie("Garden State",
-                            "Zach Braff returns home for his mother's funeral",
-                            "https://upload.wikimedia.org/wikipedia/en/3/3c/Garden_State_Poster.jpg",
-                            "https://www.youtube.com/watch?v=u82n0e1mgmQ")
+                           "Zach Braff returns home for his mother's funeral",
+                           "https://upload.wikimedia.org/wikipedia/en/3/3c/Garden_State_Poster.jpg",
+                           "https://www.youtube.com/watch?v=u82n0e1mgmQ")
 
 into_the_wild = media.Movie("Into the wild",
                             "A young man leaves society",
@@ -27,9 +28,12 @@ into_the_wild = media.Movie("Into the wild",
                             "https://www.youtube.com/watch?v=g7ArZ7VD-QQ")
 
 the_godfather = media.Movie("The Godfather",
-                            "A mob boss transfers the family business to his son",
+                            "A mob boss gives the family business to his son",
                             "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
                             "https://www.youtube.com/watch?v=sY1S34973zA")
 
+# Create an array containing all movies
 movies = [toy_story, avatar, eternal_sunshine, garden_state, into_the_wild, the_godfather]
+
+# generate an HTML document to display the movies
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,13 +1,16 @@
 import webbrowser
 
+
 class Movie():
     """ This class provides a way to store movie related information """
 
     def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+        """ Constructs new instance of Movie """
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
+        """ Opens youtube trailer in a web browser """
         webbrowser.open(self.trailer_youtube_url)
